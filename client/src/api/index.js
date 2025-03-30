d806b5002e1f43516a240d8902cfe3999d9fd897
Suchit <gourkhedesuchit@gmail.com>
2025-03-30 18:46:43 +0530

    backcheck

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://fitness-tracker-a1hp.onrender.com/",
+  baseURL: "https://fitness-tracker-a1hp.onrender.com",
 });
 
 export const UserSignUp = async (data) => API.post("/user/signup", data);
